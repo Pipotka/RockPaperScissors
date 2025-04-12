fbbeca29b6a8aa35134b14f46320a21007d3293f
Pipotka <157719919+Pipotka@users.noreply.github.com>
2025-04-12 12:15:59 +0300

    end

--- app/src/main/java/com/example/rockpaperscissors/Game.kt
+++ app/src/main/java/com/example/rockpaperscissors/Game.kt
@@ -1,24 +1,29 @@
 package com.example.rockpaperscissors
 
+import GameItems
+import android.view.View
+import compareChoices
 import kotlin.random.Random
 
 public class Game {
 
-    companion object {
-        public fun GetItemForComputer() : GameItems {
-            return GetItemByNumber(Random.nextInt(0, 5))
-        }
+    public fun determineTheWinner(UseItem : GameItems, BotItem : GameItems)  : Int{
+        return compareChoices(UseItem, BotItem)
+    }
 
+    public fun getItemForBot() : GameItems{
+        var result  = Random.nextInt(0, 5)
+        return getItemByNumber(result)
+    }
 
-        private fun GetItemByNumber(Number : Int) : GameItems{
-            return when(Number){
-                0 -> GameItems.ROCK
-                1 -> GameItems.PAPER
-                2 -> GameItems.SCISSORS
-                3 -> GameItems.LIZARD
-                4 -> GameItems.SPOCK
-                else -> GameItems.NONE
-            }
+    public fun getItemByNumber(Number: Int) : GameItems{
+        return when (Number){
+            0 -> GameItems.ROCK
+            1 -> GameItems.PAPER
+            2 -> GameItems.SCISSORS
+            3 -> GameItems.LIZARD
+            4 -> GameItems.SPOCK
+            else -> GameItems.NONE
         }
     }
 }
--- app/src/main/java/com/example/rockpaperscissors/GameItems.kt
+++ app/src/main/java/com/example/rockpaperscissors/GameItems.kt
@@ -1,10 +1,37 @@
-package com.example.rockpaperscissors
+public enum class GameItems {
+    ROCK {
+        override val defeats: List<GameItems>
+            get() = listOf(SCISSORS, LIZARD)
+    },
+    PAPER {
+        override val defeats: List<GameItems>
+            get() = listOf(ROCK, SPOCK)
+    },
+    SCISSORS {
+        override val defeats: List<GameItems>
+            get() = listOf(PAPER, LIZARD)
+    },
+    LIZARD {
+        override val defeats: List<GameItems>
+            get() = listOf(PAPER, SPOCK)
+    },
+    SPOCK {
+        override val defeats: List<GameItems>
+            get() = listOf(SCISSORS, ROCK)
+    },
 
-enum class GameItems {
-    ROCK,
-    PAPER,
-    SCISSORS,
-    LIZARD,
-    SPOCK,
-    NONE
+    NONE {
+        override val defeats: List<GameItems>
+            get() = listOf()
+    };
+
+    abstract val defeats: List<GameItems>
+}
+
+public fun compareChoices(userChoice: GameItems, botChoice: GameItems): Int {
+    return when {
+        userChoice == botChoice -> 0
+        botChoice in userChoice.defeats -> 1
+        else -> -1
+    }
 }
